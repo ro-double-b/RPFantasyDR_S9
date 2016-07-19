7f044a0574fce036b9c9396befa378b5ef9fe568
Robb Marsee <robb.marsee@gamil.com>
2016-07-19 00:04:13 -0700

    "use PeerJS to establish a webRTC connection between two different browsers"

--- client/app.js
+++ client/app.js
@@ -1,6 +1,23 @@
-var connect
 var peer = new Peer({key: 'db4iiswfn8doyldi'})
 
+// // generates a unique id for each user
 peer.on('open', function(id) {
   $('#host_id').text(id)
-})
+});
+
+peer.on('connection', connect);
+
+// function for what happens on establishment of connection
+function connect(conn) {
+  $('#guest_id').val(conn.peer);
+};
+
+// jQuery event that established connection
+$().ready(function() {
+  $('#connect').click(function() {
+    var c = peer.connect($('#guest_id').val())
+    c.on('open', function() {
+      connect(c)
+    });
+  });
+});
