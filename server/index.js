00a1daacacd97ff4c8a51bc1e1b9483ba0abd638
Greg Roche <grroche.us@gmail.com>
2016-07-19 16:11:53 -0700

    Implement socket.io, seems to work
    Former-commit-id: e8dfc710c0603fdabed05ae5dec02a5fde2a3103

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+// http://stackoverflow.com/questions/10231688/node-js-socket-io-vs-express-static
+var http = require('http');
+var server = http.createServer(app);
+var path = require('path');
+var io = require('socket.io').listen(server);
+
+
+app.use(express.static(__dirname + "/../client-mockup"))
+
+// app.get('/', function(req, res) {
+// })
+
+// app.get('/', function(req, res) {
+// 	res.sendFile(path.resolve(__dirname + "/../client-mockup/index.html"));
+// });
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+
+	socket.on('chat message', function(msg) {
+		console.log(msg);
+		io.emit('chat message', msg);
+	});
+});
+
+
+
+server.listen(3000); //, function() {
+	console.log('Listening on port 3000');
+// });
