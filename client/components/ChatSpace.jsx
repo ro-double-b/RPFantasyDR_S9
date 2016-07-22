6d2ebabf082559e6775adbb0a417885aadc5fd19
Greg Roche <grroche.us@gmail.com>
2016-07-21 17:12:50 -0700

    Implement socket.io chat functionality and server into react skeleton
    Former-commit-id: b5669085376ccea19b224cfe0e426b16f273405d

--- client/components/ChatSpace.jsx
+++ client/components/ChatSpace.jsx
@@ -4,30 +4,85 @@ import Message from './Message.jsx';
 class ChatSpace extends React.Component {
 	constructor(props) {
 		super(props);
+		var socket = io();
 
+		this.state = {
+			message: "",
+			messages: [
+			{className: "other", text:"Oh my god I love this part!"},
+			{className: "me", text:"HAHAHA"}
+			],
+			socket: socket
+		};
+
+		// this.setState({
+		// 	socket: socket
+		// })
+    // grab form submission data and emit message to server.
+    // also post locally, and use broadcast tag so you don't pick up your own message
+
+    socket.on('chat message', function(msg) {
+      console.log('Recieved message from server: ', msg);
+      // this.state.messages.push({className:"other", text: msg});
+      this.setState({
+      	messages: this.state.messages.concat({className:"other", text: msg})
+      })
+    }.bind(this));
 		// { this.props.messages.map((message) => 
 		// 						<Message message={ message }} /> 
 		// 					)
 		// 				}
 	}
 
+	// componentdid mount
+	// socket instantiated here
+	// listener to get messages incoming
 
+	handleChange (event) {
+		console.log(event.target.value);
+		this.setState({
+			message: event.target.value
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		console.log(this.state.message);
+		this.state.socket.emit('chat message', this.state.message);		
+		this.setState({
+			messages: this.state.messages.concat({className:"me", text: this.state.message}),
+			message: ''
+		});
+	}
+
+	// componentDidMount() {
+	// 	var socket = io();
+
+	// 	this.setState({
+	// 		socket: socket
+	// 	})
+ //    // grab form submission data and emit message to server.
+ //    // also post locally, and use broadcast tag so you don't pick up your own message
+
+ //    socket.on('chat message', function(msg) {
+ //      console.log('Recieved message from server: ', msg);
+ //      this.state.messages.push({className:"other", text: msg})
+ //    }.bind(this));
+	// }
 
 	render() {
 		return (
 			<div className="chat-space">
 		    <div className="chat-container">
 		      <ul>
-		        <li className="other">Oh my god I love this part!</li>
-		        <li className="me">HAHAHA</li>
-		        <li className="me">He's the best</li>
-		        <li className="other">Wait, gonna get some food</li>
-		        <li className="other">Pause for a sec</li>
-		        <li className="me">K</li>
+		        {this.state.messages.map((message) => <Message message={ message } /> )}
 		      </ul>
 		    </div>
-		    <form action="">
-		      <input id="m" autoComplete="off" /><button>Send</button>
+		    <form onSubmit={ this.handleSubmit.bind(this) }>
+		      <input type="text" id="m" value={ this.state.message } autoComplete="off" onChange= { this.handleChange.bind(this) } placeholder="Send a message..." />
+		      <input type="submit" value="Submit" />
 		    </form>
 		  </div>
 		);
