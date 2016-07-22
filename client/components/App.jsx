033fb9f987b1c72f0f6873414aa5fa186a1b727c
rhanzawa <rhanzawa@gmail.com>
2016-07-22 01:04:36 -0700

    Move socket.io instantation to index.jsx Now passed down as a prop to ChatSpace
    Former-commit-id: 97e4795cfb3c84283c2ec680d191fcb16c7c1c93

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+
 import Video from "./Video.jsx";
 import ChatSpace from "./ChatSpace.jsx";
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
-		// this.state({
-		// 	messages: [] //needs className (me or other) and message body
-		// });
-
 	}
 
 	componentDidMount() {
@@ -43,11 +39,11 @@ class App extends React.Component {
 		return (
 			<div className="wrapper">
 				<Video />
-				<ChatSpace />
+				<ChatSpace socket={this.props.socket} />
 			</div>
 		)
 	}
 }
 
-export default App;
 
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
 
 import App from './components/App.jsx';
 
+const socket = io();
+
 ReactDOM.render(
-	<App hello="hello" />,
+	<App socket={socket} />,
 	document.getElementById('app')
 );
