2c52d43ca8eb5e1370af98879a9618baa6e32634
rhanzawa <rhanzawa@gmail.com>
2016-07-25 10:42:24 -0700

    Refactor Links.jsx to display prop as link
    Former-commit-id: de10b99c0c6242273fd5ddd98c18ed2ca90fb74a

--- client/components/Link.jsx
+++ client/components/Link.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Link = () => (
+const Link = (props) => (
   <div id="link">
     <div id="link-message">
-      YAY
+      Send your friend the following link: http://localhost:3000/?id={props.myId}
     </div>
   </div>
 );
+
+Link.propTypes = {
+  myId: React.PropTypes.string,
+};
 
 export default Link;
