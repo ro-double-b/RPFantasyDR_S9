035df014bddecc9e207b88fd1c590007bc3b2ed7
rhanzawa <rhanzawa@gmail.com>
2016-07-24 19:56:58 -0700

    Refactor Link page into separate component
    Former-commit-id: c4641361b48eea0ffd9aa6dfcb09d88a9370e158

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
+import Landing from './Landing';
+import Link from './Link';
 import Video from "./Video.jsx";
 import ChatSpace from "./ChatSpace.jsx";
-import Landing from './Landing';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,20 +21,14 @@ class App extends React.Component {
     return (
       <div>
         <Landing />
-
-        // link page
-        <div id="link">
-          <div id="link-message">
-            YAY
-          </div>
-        </div>
+        <Link />
         <div className="wrapper">
           <Video />
           <ChatSpace socket={this.props.socket} />
           <button id="start-stream">Start Stream</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ client/components/Link.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Link = () => (
+  <div id="link">
+    <div id="link-message">
+      YAY
+    </div>
+  </div>
+);
+
+export default Link;
