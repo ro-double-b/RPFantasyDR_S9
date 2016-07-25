8839afc30497e370db4b6662053b6f130fee05f3
rhanzawa <rhanzawa@gmail.com>
2016-07-25 10:40:07 -0700

    Refactor application logic into App.jsx
    Former-commit-id: e15de4e6b2e079c900c2787c1314c9602188b37f

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,20 +5,103 @@ import Link from './Link';
 import Video from "./Video.jsx";
 import ChatSpace from "./ChatSpace.jsx";
 
+import { getMyId, establishPeerConnection } from '../lib/webrtc';
+import readFile from '../lib/fileReader';
+import appendChunk from '../lib/mediaSource';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setFile = this.setFile.bind(this);
+
+    const params = new URLSearchParams(location.search.slice(1));
+    const isSource = !params.has('id');
+
+    this.state = {
+      isSource,
+      file: null,
+      myId: null,
+      peerId: params.get('id'),
+      showLanding: isSource,
+      showLink: isSource,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.isSource) {
+      this.initAsSource();
+    } else {
+      this.initAsReceiver(this.state.peerId);
+    }
+  }
+
+  setFile(e) {
+    this.setState({
+      file: e.target.files[0],
+      showLanding: false,
+    });
+  }
+
+  initAsSource() {
+    // Act as source: display a link that may be sent to a receiver
+    getMyId().then((myId) => {
+      this.setState({
+        myId,
+      });
+    });
+
+    establishPeerConnection().then((conn) => {
+      // Now connected to receiver as source
+
+      // Remove the link display
+      this.setState({
+        showLink: false,
+      });
+
+      // Read in the file from disk.
+      // For each chunk, append it to the local MediaSource and send it to the other peer
+      const video = document.querySelector('.video');
+      readFile(this.state.file, (chunk) => {
+        appendChunk(chunk, video);
+        conn.send(chunk);
+      });
+    })
+    .catch(console.error.bind(console));
+  }
+
+  initAsReceiver(peerId) {
+    establishPeerConnection(peerId).then((conn) => {
+      // Now connected to source as receiver
+
+      // Listen for incoming video data from source
+      conn.on('data', (data) => {
+        if (typeof data === 'string') {
+          console.log(data);
+        } else {
+          // Append each received ArrayBuffer to the local MediaSource
+          const video = document.querySelector('.video');          
+          appendChunk(data, video);
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <Landing />
-        <Link />
+        {this.state.showLanding ? <Landing setFile={this.setFile} /> : null}
+        {this.state.showLink ? <Link myId={this.state.myId} /> : null}
         <div className="wrapper">
-          <Video />
+          <Video socket={this.props.socket} />
           <ChatSpace socket={this.props.socket} />
-          <button id="start-stream">Start Stream</button>
         </div>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  socket: React.PropTypes.object.isRequired,
+};
 
 export default App;
