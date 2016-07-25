c97130a8217fc540827d05c952c4ddcd51dbb263
rhanzawa <rhanzawa@gmail.com>
2016-07-25 10:41:24 -0700

    Refactor Landing.jsx into a stateless component
    Former-commit-id: 2e692d92c5961deb3f8c91df1f4cbe569e58daeb

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,44 +1,21 @@
 import React from 'react';
 
-class Landing extends React.Component {
-  constructor(props) {
-    super(props);
+const Landing = (props) => (
+  <div id="landing">
+    <div className="landing-left">
+      <p className="landing-logo">ReelTime</p>
+      <p className="landing-logo small">Watch tv with your friends in your underwear</p>
+    </div>
+    <div className="landing-right">
+      <div className="landing-top-right ">
+        <input type="file" id="files" className="landing-circle" name="file" onChange={props.setFile} />
+      </div>
+    </div>
+  </div>
+);
 
-    this.state = {
-      hidden: false,
-      file: '',
-    };
-
-    this.handleFileSelect = this.handleFileSelect.bind(this);
-  }
-
-  handleFileSelect(e) {
-    this.setState({
-      file: e.target.files[0],
-      hidden: true,
-    });
-  }
-
-  render() {
-    if (this.state.hidden) {
-      console.log(this.state);
-      return (<div></div>);
-    } else {
-      return (
-        <div id="landing">
-          <div className="landing-left">
-            <p className="landing-logo">ReelTime</p>
-            <p className="landing-logo small">Watch tv with your friends in your underwear</p>
-          </div>
-          <div className="landing-right">
-            <div className="landing-top-right ">
-              <input type="file" id="files" className="landing-circle" name="file" onChange={this.handleFileSelect} />
-            </div>
-          </div>
-        </div>
-      );
-    }
-  }
-}
+Landing.propTypes = {
+  setFile: React.PropTypes.func.isRequired,
+};
 
 export default Landing;
