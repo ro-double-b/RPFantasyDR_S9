24ff085fb09a261f419076a2c1c39722961d13ea
rhanzawa <rhanzawa@gmail.com>
2016-07-25 14:50:52 -0700

    Implement video streaming

---
+++ client/components/VideoChat.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { establishPeerCall } from '../lib/webrtc';
+
+class VideoChat extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      localStream: null,
+    };
+
+    this.setUpVideoStream = this.setUpVideoStream.bind(this);
+  }
+
+  componentDidMount() {
+    const constraints = {
+      audio: true,
+      video: true,
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(this.setUpVideoStream)
+      .catch(console.error.bind(console));
+  }
+
+  setUpVideoStream(localStream) {
+    const localVideo = document.querySelector('.local-video');
+    localVideo.srcObject = localStream;
+
+    establishPeerCall(localStream, this.props.isSource ? null : this.props.peerId)
+      .then((remoteStream) => {
+        console.log(remoteStream);
+
+        const remoteVideo = document.querySelector('.remote-video');
+        remoteVideo.srcObject = remoteStream;
+      })
+      .catch(console.error.bind(console));
+  }
+
+  render() {
+    return (
+      <div>
+        <video className="local-video" autoPlay></video>
+        <video className="remote-video" autoPlay></video>
+      </div>
+    );
+  }
+}
+
+VideoChat.propTypes = {
+  isSource: React.PropTypes.bool.isRequired,
+  peerId: React.PropTypes.string,
+};
+
+export default VideoChat;
