72606463e414d8e11b07e35db7b82d56d99747da
rhanzawa <rhanzawa@gmail.com>
2016-07-25 14:50:46 -0700

    Add establishPeerCall function in webrtc.js

--- client/lib/webrtc.js
+++ client/lib/webrtc.js
@@ -38,11 +38,56 @@ const establishPeerConnection = (sourceId) => new Promise((resolve, reject) => {
     }
   };
 
-  peer.on('open', connect);
-  peer.on('error', reject);
+  if (peer.disconnected) {
+    peer.on('open', connect);
+    peer.on('error', reject);
+  } else {
+    connect();
+  }
+});
+
+// Returns a Promise that is resolved with an active peer.js MediaConnection.
+// A MediaStream in active state must be provided as the first argument.
+// If sourceId is specified, this will connect to an existing peer with that source ID.
+// If sourceId is not specified, this will listen for an incoming connection.
+const establishPeerCall = (mediaStream, sourceId) => new Promise((resolve, reject) => {
+  const connect = () => {
+    if (sourceId) {
+      const call = peer.call(sourceId, mediaStream); ////////////////////////////
+
+      call.on('stream', (stream) => {
+        console.log('RTC call established - acting as receiver');
+        resolve(stream);
+      });
+
+      call.on('error', (error) => {
+        reject(error);
+      });
+    } else {
+      peer.on('call', (call) => {
+        call.answer(mediaStream); /////////////////////////////
+        call.on('stream', (stream) => {
+          console.log('RTC call established - acting as source');
+          resolve(stream);
+        });
+      });
+
+      peer.on('error', (error) => {
+        reject(error);
+      });
+    }
+  };
+
+  if (peer.disconnected) {
+    peer.on('open', connect);
+    peer.on('error', reject);
+  } else {
+    connect();
+  }
 });
 
 export {
   getMyId,
   establishPeerConnection,
+  establishPeerCall,
 };
