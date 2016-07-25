363c2593a458f3b10441ab77789ba0881c5f995d
rhanzawa <rhanzawa@gmail.com>
2016-07-25 10:43:12 -0700

    Add video sync feature
    Former-commit-id: d5f97535a562b20aedc997e05b455bcdb8aec999

--- server/index.js
+++ server/index.js
@@ -23,10 +23,21 @@ io.on('connection', (socket) => {
     console.log('A user disconnected with socket id', socket.id);
   });
 
-
+  // Chat messaging events
   socket.on('chat message', (msg) => {
     console.log(msg);
     socket.broadcast.emit('chat message', msg);
+  });
+
+  // Video sync events
+  socket.on('play', (time) => {
+    console.log('Play command recieved');
+    socket.broadcast.emit('play', time);
+  });
+
+  socket.on('pause', (time) => {
+    console.log('Pause command recieved');
+    socket.broadcast.emit('pause', time);
   });
 });
 
