d1a8ad1931250d981f79651b751757611d9d48e2
Robb Marsee <robb.marsee@gamil.com>
2017-03-11 20:57:04 -0800

    update server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + './../client'))
+
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
 app.listen(3000, function () {
 
   console.log('Example app listening on port 3000!')
