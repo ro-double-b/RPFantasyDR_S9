42bfbffedad1a7ae609cf263351a589affa88375
Robb Marsee <robb.marsee@gamil.com>
2017-03-14 14:25:56 -0700

    broke up different views for logged in and logged out

--- client/app/app.js
+++ client/app/app.js
@@ -5,17 +5,47 @@ angular.module('fantasyDragRace', [
   .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
+    .state('login', {
+      url: '/',
+      views: {
+        '': { templateUrl: './app/views/index.html',
+      },
+        'landing@login': {
+          templateUrl: './app/partials/signedOut/landing.html',
+          controller: 'AuthController',
+        },
+        'navbar@login': {
+          templateUrl: './app/partials/signedOut/navbar.html',
+        },
+        'about@login': {
+          templateUrl: './app/partials/about.html',
+        },
+        'rules@login': {
+          templateUrl: './app/partials/rules.html',
+        },
+        'selection@login': {
+          templateUrl: './app/partials/signedOut/selection.html',
+          controller: 'SelectionController',
+        },
+        'rankings@login': {
+          templateUrl: './app/partials/rankings.html',
+        },
+        'aboutme@login': {
+          templateUrl: './app/partials/aboutme.html',
+        },
+      },
+    })
     .state('home', {
       url: '/home',
       views: {
-        '': { templateUrl: './app/views/home.html',
+        '': { templateUrl: './app/views/index.html',
       },
         'landing@home': {
-          templateUrl: './app/partials/landing.html',
+          templateUrl: './app/partials/signedIn/landing.html',
           controller: 'AuthController',
         },
         'navbar@home': {
-          templateUrl: './app/partials/navbar.html',
+          templateUrl: './app/partials/signedIn/navbar.html',
         },
         'about@home': {
           templateUrl: './app/partials/about.html',
@@ -24,7 +54,7 @@ angular.module('fantasyDragRace', [
           templateUrl: './app/partials/rules.html',
         },
         'selection@home': {
-          templateUrl: './app/partials/selection.html',
+          templateUrl: './app/partials/signedIn/selection.html',
           controller: 'SelectionController',
         },
         'rankings@home': {
