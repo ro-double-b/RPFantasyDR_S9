93c1806039c2227c0def8812ee23c261db3bc14d
Robb Marsee <robb.marsee@gamil.com>
2017-03-14 11:14:53 -0700

    establish connection to authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,20 @@
 const db = require('../database/db.js');
-function signup(req, res) {
 
+function createUser(name, username, password, email) {
+  return db.User.create({
+    name,
+    username,
+    password,
+    email,
+  });
+}
+
+function signup(req, res) {
+  console.log(req.body)
 }
 
 function login(req, res) {
-
+  console.log(req.body)
 }
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -5,19 +5,20 @@ const router = require('./routes.js');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(`${__dirname}./../client`));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.use(express.static(`${__dirname}./../client`));
 // app.use('/scripts', express.static(`${__dirname}./../node_modules`));
 
 // app.get('/', (req, res) => {
   // res.send('./../client');
 // });
-app.use('/', router)
+app.use('/', router);
 
-app.listen(port)
-console.log(`Listening on port ${port}`)
+app.listen(port);
+console.log(`Listening on port ${port}`);
 
