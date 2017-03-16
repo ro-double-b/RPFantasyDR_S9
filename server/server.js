4f58d64ee7a2dec8bef86d16da41831150879d95
Robb Marsee <robb.marsee@gamil.com>
2017-03-16 16:56:18 -0700

    update server for heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,16 +6,16 @@ const router = require('./routes.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 app.use(express.static(`${__dirname}./../client`));
 
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true,
-}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
 }));
 
 app.use('/', router);
