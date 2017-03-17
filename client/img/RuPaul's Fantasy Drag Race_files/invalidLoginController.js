3aac1a5d613f83dd9527f834cd69538e2156fda6
Robb Marsee <robb.marsee@gamil.com>
2017-03-16 17:55:19 -0700

    update files for uppercase problems on heroku

--- client/img/RuPaul's Fantasy Drag Race_files/invalidLoginController.js
+++
@@ -1,7 +0,0 @@
-angular.module('fantasyDragRace')
-.controller('InvalidLogin', function($scope, $modalInstance) {
-  $scope.selectedLanguages = [];
-  $scope.close = function() {
-    $modalInstance.dismiss('cancel');
-  };
-});
