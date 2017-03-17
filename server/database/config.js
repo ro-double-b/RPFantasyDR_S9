c5310851ba0424a49696356f46919a86d01dd1b9
Robb Marsee <robb.marsee@gamil.com>
2017-03-17 12:59:47 -0700

    update for username and passworkd

--- server/database/config.js
+++ server/database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   database: process.env.DATABASE_URL || 'RPFantasyDR',
-  username: 'robb',
-  password: 'AAeeII11',
+  username: 'xzqklqevjsyqoy',
+  password: '611387d0a04c586d81c960b23c5ecb5f85b051fbe1f8fadf5aa3cb4c28fd7483',
   host: process.env.DATABASE_URL ? process.env.DATABASE_URL.split(':')[2] : 'localhost',
 };
