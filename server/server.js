dbbb115e42a49e7e20e046545a91dbd67cc86764
Robb Marsee <robb.marsee@gamil.com>
2017-03-17 13:29:52 -0700

    udpate server so it successfully connects to the heroku database

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const router = require('./routes.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log(process.env.DATABASE_URL, '============================================================================================================================================================================================================================================================================================================================================================================================================================================================================');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -24,21 +22,3 @@ app.use('/', router);
 
 app.listen(port);
 console.log(`Listening on port ${port}`);
-
-// app.use('/scripts', express.static(`${__dirname}./../node_modules`));
-
-// app.get('/', (req, res) => {
-  // res.send('./../client');
-// });
-
-var pg = require('pg');
-
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  //  console.log(err+"!!!!!!!!!!!!!!!");
-  // client.query('SELECT * FROM your_table', function(err, result) {
-  //   done();
-  //   if(err) return console.error(err);
-  //   console.log(result.rows);
-  // });
-});
