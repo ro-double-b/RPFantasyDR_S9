1e8d893c445692f6bce4955362b1bcadfbddd753
Robb Marsee <robb.marsee@gamil.com>
2017-03-20 10:16:28 -0700

    update for heroku push

--- server/database/config.js
+++ server/database/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   database: process.env.DATABASE_URL || 'RPFantasyDR',
-  username: null,
-  password: null,
-  // username: 'xzqklqevjsyqoy',
-  // password: '611387d0a04c586d81c960b23c5ecb5f85b051fbe1f8fadf5aa3cb4c28fd7483',
+  // username: null,
+  // password: null,
+  username: 'xzqklqevjsyqoy',
+  password: '611387d0a04c586d81c960b23c5ecb5f85b051fbe1f8fadf5aa3cb4c28fd7483',
   host: process.env.DATABASE_URL ? process.env.DATABASE_URL.split(':')[2] : 'localhost',
 };
